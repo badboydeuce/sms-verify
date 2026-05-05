da455cce47f10dd94d1f0391f7888e2563befdac
badboydeuce <badboydeuce@bill.com>
2026-05-05 18:41:13 +0100

    Update main.py

--- app/bot/keyboards/main.py
+++ app/bot/keyboards/main.py
@@ -1,12 +1,10 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-
-def main_menu():
-    return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="🛒 Buy Number")],
-            [KeyboardButton(text="👤 My Profile"), KeyboardButton(text="➕ Fund Wallet")],
-            [KeyboardButton(text="📞 Contact Support")]
-        ],
-        resize_keyboard=True
-    )
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🛒 Buy Number")],
+        [KeyboardButton(text="👤 My Profile"), KeyboardButton(text="➕ Fund Wallet")],
+        [KeyboardButton(text="📞 Contact Support")]
+    ],
+    resize_keyboard=True
+)
