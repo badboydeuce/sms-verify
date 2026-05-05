4c28067b74d88ab3523979007b2226fa703136bb
badboydeuce <badboydeuce@bill.com>
2026-05-05 01:37:54 +0100

    Update bot.py

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,21 +1,32 @@
-from aiogram import Bot, Dispatcher
 import asyncio
 import os
+
+from aiogram import Bot, Dispatcher
 
 from app.bot.handlers import start, buy
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-dp.include_router(start.router)
-dp.include_router(buy.router)
-
 
 async def main():
+
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    # =========================
+    # REGISTER ROUTERS
+    # =========================
+    dp.include_router(start.router)
+    dp.include_router(buy.router)
+
+    print("🚀 Bot is running...")
+
     await dp.start_polling(bot)
 
 
 def run_bot():
     asyncio.run(main())
+
+
+if __name__ == "__main__":
+    run_bot()
