b97ab985a5e48e730bc19882cfb5ac781e3ad589
badboydeuce <badboydeuce@bill.com>
2026-05-07 10:01:18 -0700

    Update admin.py

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -2,7 +2,7 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from bot.filters.admin import (
+from bot.handlers.filters.admin import (
     AdminFilter
 )
 
