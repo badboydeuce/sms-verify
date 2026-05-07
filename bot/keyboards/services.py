b5563d65a7f3f1d86e8f993a9be914de68b4d31a
badboydeuce <badboydeuce@bill.com>
2026-05-07 07:58:13 -0700

    Create services.py

---
+++ bot/keyboards/services.py
@@ -0,0 +1,36 @@
+from aiogram.utils.keyboard import (
+    InlineKeyboardBuilder
+)
+
+from bot.callback_factories.buy import (
+    BuyCallback
+)
+
+
+def services_keyboard(
+    services,
+    country_id
+):
+
+    kb = InlineKeyboardBuilder()
+
+    for service in services[:40]:
+
+        kb.button(
+            text=(
+                f"{service['name']} "
+                f"₦{service['price']}"
+            ),
+
+            callback_data=BuyCallback(
+                action="service",
+                value=(
+                    f"{country_id}:"
+                    f"{service['id']}"
+                )
+            ).pack()
+        )
+
+    kb.adjust(1)
+
+    return kb.as_markup()
