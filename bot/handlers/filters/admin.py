f76fa3beb6deafe47a1321b186b7e552661d1366
badboydeuce <badboydeuce@bill.com>
2026-05-06 22:17:10 -0700

    Create admin.py

---
+++ bot/handlers/filters/admin.py
@@ -0,0 +1,11 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+
+from bot.config import ADMIN_IDS
+
+
+class AdminFilter(BaseFilter):
+
+    async def __call__(self, message: Message):
+
+        return message.from_user.id in ADMIN_IDS
