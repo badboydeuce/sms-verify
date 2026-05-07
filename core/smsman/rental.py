ad12a2eb2fd8699898e5804d34e7c6e9e5fc2153
badboydeuce <badboydeuce@bill.com>
2026-05-06 22:36:47 -0700

    Create rental.py

---
+++ core/smsman/rental.py
@@ -0,0 +1,73 @@
+import httpx
+import os
+
+BASE_URL = "https://api.sms-man.com/control/rent-api"
+
+TOKEN = os.getenv("SMSMAN_TOKEN")
+
+
+class SMSManRental:
+
+    @staticmethod
+    async def get_limits(country_id, rent_type, time):
+
+        async with httpx.AsyncClient() as client:
+
+            response = await client.get(
+                f"{BASE_URL}/limits",
+                params={
+                    "token": TOKEN,
+                    "country_id": country_id,
+                    "type": rent_type,
+                    "time": time
+                }
+            )
+
+            return response.json()
+
+    @staticmethod
+    async def get_number(country_id, rent_type, time):
+
+        async with httpx.AsyncClient() as client:
+
+            response = await client.get(
+                f"{BASE_URL}/get-number",
+                params={
+                    "token": TOKEN,
+                    "country_id": country_id,
+                    "type": rent_type,
+                    "time": time
+                }
+            )
+
+            return response.json()
+
+    @staticmethod
+    async def get_sms(request_id):
+
+        async with httpx.AsyncClient() as client:
+
+            response = await client.get(
+                f"{BASE_URL}/get-sms",
+                params={
+                    "token": TOKEN,
+                    "request_id": request_id
+                }
+            )
+
+            return response.json()
+
+    @staticmethod
+    async def get_all_sms(request_id):
+
+        async with httpx.AsyncClient() as client:
+
+            response = await client.get(
+                f"{BASE_URL}/get-all-sms",
+                params={
+                    "token": TOKEN,
+                    "request_id": request_id
+                }
+            )
+
+            return response.json()
