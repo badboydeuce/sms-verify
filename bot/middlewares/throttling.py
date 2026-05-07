fbe527441fb14eba36ea30f6b40e4f2a77c96dde
badboydeuce <badboydeuce@bill.com>
2026-05-07 07:13:01 -0700

    Create throttling.py

---
+++ bot/middlewares/throttling.py
@@ -0,0 +1,48 @@
+import time
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+
+
+class ThrottlingMiddleware(
+    BaseMiddleware
+):
+
+    cache = {}
+
+    RATE_LIMIT = 1
+
+    async def __call__(
+        self,
+        handler,
+        event: TelegramObject,
+        data
+    ):
+
+        user = data.get(
+            "event_from_user"
+        )
+
+        if not user:
+            return await handler(
+                event,
+                data
+            )
+
+        now = time.time()
+
+        last_request = self.cache.get(
+            user.id,
+            0
+        )
+
+        if now - last_request < self.RATE_LIMIT:
+
+            return
+
+        self.cache[user.id] = now
+
+        return await handler(
+            event,
+            data
+        )
