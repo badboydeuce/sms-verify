67f874891140dcad95612f9b05f1cf10dda7c86e
badboydeuce <badboydeuce@bill.com>
2026-05-07 07:56:46 -0700

    Create countries.py

---
+++ bot/keyboards/countries.py
@@ -0,0 +1,29 @@
+from aiogram.utils.keyboard import (
+    InlineKeyboardBuilder
+)
+
+from bot.callback_factories.buy import (
+    BuyCallback
+)
+
+
+def countries_keyboard(
+    countries
+):
+
+    kb = InlineKeyboardBuilder()
+
+    for country in countries[:40]:
+
+        kb.button(
+            text=f"🌍 {country['name']}",
+
+            callback_data=BuyCallback(
+                action="country",
+                value=str(country["id"])
+            ).pack()
+        )
+
+    kb.adjust(2)
+
+    return kb.as_markup()
