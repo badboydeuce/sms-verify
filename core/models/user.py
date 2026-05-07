3d041c519137a64b284fcb263492b061237d9602
badboydeuce <badboydeuce@bill.com>
2026-05-06 22:33:14 -0700

    Create user.py

---
+++ core/models/user.py
@@ -0,0 +1,51 @@
+from sqlalchemy import (
+    BigInteger,
+    String,
+    Boolean,
+    Numeric,
+    DateTime
+)
+
+from sqlalchemy.orm import mapped_column, Mapped
+from datetime import datetime
+
+from core.database.base import Base
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    telegram_id: Mapped[int] = mapped_column(
+        BigInteger,
+        unique=True,
+        index=True
+    )
+
+    username: Mapped[str | None] = mapped_column(
+        String(255),
+        nullable=True
+    )
+
+    balance: Mapped[float] = mapped_column(
+        Numeric(12, 2),
+        default=0.00
+    )
+
+    is_admin: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
