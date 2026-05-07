5354d801a81bafa30d3aab58650d7ad5f59ecdf3
badboydeuce <badboydeuce@bill.com>
2026-05-06 22:32:26 -0700

    Create init_db.py

---
+++ core/database/init_db.py
@@ -0,0 +1,12 @@
+from core.database.session import engine
+from core.database.base import Base
+
+from core.models.user import User
+from core.models.order import Order
+from core.models.transaction import Transaction
+from core.models.payment_transaction import PaymentTransaction
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
