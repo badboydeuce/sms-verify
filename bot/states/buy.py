37dd72e0a2b4fd2f21a2fb52cc103fe754205d47
badboydeuce <badboydeuce@bill.com>
2026-05-15 22:32:23 -0700

    Update buy.py

--- bot/states/buy.py
+++ bot/states/buy.py
@@ -6,4 +6,6 @@ from aiogram.fsm.state import State, StatesGroup
 class BuyStates(StatesGroup):
     search_country = State()
     search_service = State()
-    rental_search_country = State() 
+    rental_search_country = State()
+    fivesim_search_country = State()  # ✅ new
+    fivesim_search_service = State()  # ✅ new
